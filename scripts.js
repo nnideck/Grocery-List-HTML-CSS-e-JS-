c9365bae2792109492333f1c4f04347869cefb38
nnideck <natalia.nideck@gmail.com>
2023-09-05 23:38:14 -0300

    R03 - construindo save on localstorage

--- scripts.js
+++ scripts.js
@@ -7,39 +7,113 @@ const btnAddItem = document.querySelector(".btn-add-item");
 const ItensList = document.querySelector(".itens-list");
 const btnDelete = document.querySelector(".btn-delete");
 
-/* function check() {
-  const input = this.querySelector("input");
-  console.log(input);
-  if (input.checked) {
-    this.classList.remove("list-group-item-light");
-    this.classList.add("list-group-item-success");
-  } else {
-    this.classList.remove("list-group-item-success");
-    this.classList.add("list-group-item-light");
+// 1 - lista para salvar tasks - OK
+// 2 - criar função para salvar tasks - STRING - OK
+// 3 - criar função para logar tasks - OK
+// 4 - criar função para handle task - OK
+// CRIAR FUNÇÃO PARA CARREGAR AS TASKS SALVAS - OK
+// 5 - adaptar função addtask para receber tbm as tarefas salvas - OK
+// 6 - adaptar a função check para salvar as tarefas com check
+// 7 - adaptar a função delete para retornar as tarefas não deletadas - OK
+
+let savedList = [
+  {
+    text: "Teste",
+    id: 1,
+    checked: false,
+  },
+];
+
+function saveItem(newItem) {
+  //pq if? toda vez que invoco a função ela de fato tem um novo item..
+  if (newItem) {
+    const savedItens = JSON.stringify(newItem);
+    localStorage.setItem("List", savedItens);
   }
 }
- */
-function addTask() {
+
+function loadItens() {
+  const loadedItens = localStorage.getItem("List");
+  const itens = JSON.parse(loadedItens);
+  return itens;
+}
+
+function setItens() {
+  const loadedItensList = loadItens();
+  if (loadedItensList) {
+    savedList = loadedItensList;
+    loadedItensList.forEach(function (item) {
+      addTask(item);
+    });
+  }
+}
+setItens();
+
+function handleAddItem() {
   const value = inputItem.value;
-  console.log(value);
+  const qntValue = inputQnt.value;
+  const unitValue = inputUnit.value;
   if (value) {
-    const newItemTask = templateItem.content.cloneNode(true);
-    newItemTask.querySelector("span").innerHTML = `${inputQnt.value} ${
-      inputUnit.value
-    } of ${value.bold()}`;
-    // newItemTask.querySelector("button").addEventListener("click", remove);
-    ItensList.insertBefore(newItemTask, ItensList.querySelector("li"));
+    const newId = Math.random() * 10000;
+    const item = {
+      text: `${qntValue} ${unitValue} of ${value}`,
+      id: newId,
+      checked: false,
+    };
+    savedList.push(item);
+    saveItem(savedList);
+    addTask(item);
+    console.log("criar", savedList);
     inputItem.value = "";
     inputQnt.value = "";
     inputUnit.value = "unit";
   }
 }
 
+function addTask(newItem) {
+  const newListItem = templateItem.content.cloneNode(true);
+  newListItem.querySelector("span").innerHTML = newItem.text;
+  //pq as tres linhas abaixo?
+  const input = newListItem.querySelector("input");
+  const li = newListItem.querySelector("li");
+  li.setAttribute("id", newItem.id);
+
+  newListItem.querySelector("button").addEventListener("click", deleteItem);
+  ItensList.appendChild(newListItem);
+  if (newItem.checked) {
+    input.setAttribute("checked", true);
+  }
+  li.addEventListener("click", check);
+}
+
 function deleteItem() {
   const item = document.querySelector(".list-item");
+  const id = item.id;
+  savedList = savedList.filter(function (eachItem) {
+    return eachItem.id != id;
+  });
+  saveItem(savedList);
   item.remove();
 }
 
-btnAddItem.addEventListener("click", addTask);
+function check() {
+  const input = this.querySelector("input");
+  const item = document.querySelector(".list-item");
+  const id = item.id;
+  if (input.checked) {
+    savedList.forEach(function (eachItem) {
+      if (eachItem.id == id) {
+        eachItem.checked = true;
+      }
+    });
+    //ñ entendi a necessidade do else
+  } /*  else {
+    savedList.forEach(function (eachItem) {
+      if (eachItem.id == id) {
+        eachItem.checked = false;
+      }
+    });
+  } */
+}
 
-//btnDelete.addEventListener("click", deleteItem);
+btnAddItem.addEventListener("click", handleAddItem);
